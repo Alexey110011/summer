9ec1df7471b5e4bf313877c570fd4cb9b84ef4a9
Alexey <alleshkvch@gmail.com>
2022-05-14 13:20:10 +0300

    picture

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import {InitialState} from './InitialState'
 
 function App() {
   return (
-    
-      <div className = "App">
-      <InitialState/>
-    </div>
-    
-  );
+       <div className = "App">
+          <InitialState/>
+       </div>
+    );
 }
 
 export default App;
